3deef835ba78f29e12c39808f8c801093389f9d0
John Sinfuego <you@example.com>
2024-04-21 21:23:28 -0400

    private variables default constructor

--- John SInfuego Alternative Language Project/John-Angelo-Sinfuego-Alternate-Language-Project/Cell.java
+++ John SInfuego Alternative Language Project/John-Angelo-Sinfuego-Alternate-Language-Project/Cell.java
@@ -6,5 +6,21 @@ import java.util.HashMap;
 
 
 public class Cell{
-    
+    private String oem;
+    private String model;
+    private Integer launchAnnounced;
+    private String launchStatus;
+    private String bodyDimensions;
+    private Float bodyWeight;
+    private String bodySim;
+    private String displayType;
+    private Float displaySize;
+    private String displayResolution;
+    private String featuresSensors;
+    private String platformOs;
+
+    // Constructors
+    public Cell() {
+        // Default constructor
+    }
 }
