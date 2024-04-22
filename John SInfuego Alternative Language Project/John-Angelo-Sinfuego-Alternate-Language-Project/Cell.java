8b4efeb666c6ea1267196215b83915cc3d19127f
John Sinfuego <you@example.com>
2024-04-21 21:24:16 -0400

    getter setters

--- John SInfuego Alternative Language Project/John-Angelo-Sinfuego-Alternate-Language-Project/Cell.java
+++ John SInfuego Alternative Language Project/John-Angelo-Sinfuego-Alternate-Language-Project/Cell.java
@@ -23,4 +23,116 @@ public class Cell{
     public Cell() {
         // Default constructor
     }
+
+    public Cell(String oem, String model, Integer launchAnnounced, String launchStatus, String bodyDimensions, Float bodyWeight, String bodySim, String displayType, Float displaySize, String displayResolution, String featuresSensors, String platformOs) {
+        this.oem = oem;
+        this.model = model;
+        this.launchAnnounced = launchAnnounced;
+        this.launchStatus = launchStatus;
+        this.bodyDimensions = bodyDimensions;
+        this.bodyWeight = bodyWeight;
+        this.bodySim = bodySim;
+        this.displayType = displayType;
+        this.displaySize = displaySize;
+        this.displayResolution = displayResolution;
+        this.featuresSensors = featuresSensors;
+        this.platformOs = platformOs;
+    }
+
+    // Getter and Setter methods for each attribute
+    public String getOem() {
+        return oem;
+    }
+
+    public void setOem(String oem) {
+        this.oem = oem;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getLaunchAnnounced() {
+        return launchAnnounced;
+    }
+
+    public void setLaunchAnnounced(Integer launchAnnounced) {
+        this.launchAnnounced = launchAnnounced;
+    }
+
+    public String getLaunchStatus() {
+        return launchStatus;
+    }
+
+    public void setLaunchStatus(String launchStatus) {
+        this.launchStatus = launchStatus;
+    }
+
+    public String getBodyDimensions() {
+        return bodyDimensions;
+    }
+
+    public void setBodyDimensions(String bodyDimensions) {
+        this.bodyDimensions = bodyDimensions;
+    }
+
+    public Float getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBodyWeight(Float bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public String getBodySim() {
+        return bodySim;
+    }
+
+    public void setBodySim(String bodySim) {
+        this.bodySim = bodySim;
+    }
+
+    public String getDisplayType() {
+        return displayType;
+    }
+
+    public void setDisplayType(String displayType) {
+        this.displayType = displayType;
+    }
+
+    public Float getDisplaySize() {
+        return displaySize;
+    }
+
+    public void setDisplaySize(Float displaySize) {
+        this.displaySize = displaySize;
+    }
+
+    public String getDisplayResolution() {
+        return displayResolution;
+    }
+
+    public void setDisplayResolution(String displayResolution) {
+        this.displayResolution = displayResolution;
+    }
+
+    public String getFeaturesSensors() {
+        return featuresSensors;
+    }
+
+    public void setFeaturesSensors(String featuresSensors) {
+        this.featuresSensors = featuresSensors;
+    }
+
+    public String getPlatformOs() {
+        return platformOs;
+    }
+
+    public void setPlatformOs(String platformOs) {
+        this.platformOs = platformOs;
+    }
 }
